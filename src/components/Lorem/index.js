9b87a64e10681ed6153352fed95b7313822e711e
Tin Nguyen <nguyenbatin290900@gmail.com>
2023-10-20 07:47:57 +0700

    UI

--- src/components/Lorem/index.js
+++ src/components/Lorem/index.js
@@ -10,20 +10,23 @@ const Lorem = () => {
         <div className='lorem'>
             <div className='lorem-item'>
                 <h2>Lorem ipsum dolor sit amet</h2>
-                <div>
+                <div className="content">
                     <img src={icon1} />
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non dui sodales, faucibus libero ut, posuere felis. Donec imperdiet suscipit accumsan. Aenean consequat condimentum velit ut tempor. Nam porta massa in metus bibendum congue. Pellentesque ultrices liquam egestas nunc at ullamcorper ultricies. Donec feugiat velit nulla, vel sodales est ullamcorper id. Aenean consequat condimentum velit ut tempor. Nam porta massa in metus bibendum congue. Pellentesque ultrices vestibulum mattis.</p>
                 </div>
-
             </div>
             <div className='lorem-item'>
                 <h2>Lorem ipsum dolor sit amet</h2>
-                <img src={icon2} />
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non dui sodales, faucibus libero ut, posuere felis. Donec imperdiet suscipit accumsan. Aenean consequat condimentum velit ut tempor. Nam porta massa in metus bibendum congue. Pellentesque ultrices liquam egestas nunc at ullamcorper ultricies. Donec feugiat velit nulla, vel sodales est ullamcorper id. Aenean consequat condimentum velit ut tempor. Nam porta massa in metus bibendum congue. Pellentesque ultrices vestibulum mattis.</p>
+                <div className="content">
+                    <img src={icon2} />
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non dui sodales, faucibus libero ut, posuere felis. Donec imperdiet suscipit accumsan. Aenean consequat condimentum velit ut tempor. Nam porta massa in metus bibendum congue. Pellentesque ultrices liquam egestas nunc at ullamcorper ultricies. Donec feugiat velit nulla, vel sodales est ullamcorper id. Aenean consequat condimentum velit ut tempor. Nam porta massa in metus bibendum congue. Pellentesque ultrices vestibulum mattis.</p>
+                </div>
             </div> <div className='lorem-item'>
                 <h2>Lorem ipsum dolor sit amet</h2>
-                <img src={icon3} />
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non dui sodales, faucibus libero ut, posuere felis. Donec imperdiet suscipit accumsan. Aenean consequat condimentum velit ut tempor. Nam porta massa in metus bibendum congue. Pellentesque ultrices liquam egestas nunc at ullamcorper ultricies. Donec feugiat velit nulla, vel sodales est ullamcorper id. Aenean consequat condimentum velit ut tempor. Nam porta massa in metus bibendum congue. Pellentesque ultrices vestibulum mattis.</p>
+                <div className="content">
+                    <img src={icon3} />
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non dui sodales, faucibus libero ut, posuere felis. Donec imperdiet suscipit accumsan. Aenean consequat condimentum velit ut tempor. Nam porta massa in metus bibendum congue. Pellentesque ultrices liquam egestas nunc at ullamcorper ultricies. Donec feugiat velit nulla, vel sodales est ullamcorper id. Aenean consequat condimentum velit ut tempor. Nam porta massa in metus bibendum congue. Pellentesque ultrices vestibulum mattis.</p>
+                </div>
             </div>
         </div>
     );
--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 
 const Menu = () => {
-
+    const [menuOpen, setMenuOpen] = useState(true);
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
     return (
         <div className='menu'>
-            <ul>
-                <li>Home</li>
-                <li>Servies</li>
-                <li>News</li>
-                <li>Blog</li>
-                <li>Contact</li>
-            </ul>
+
+            {menuOpen && (
+                <ul>
+                    <button onClick={toggleMenu} className="menu-button">
+                        Toggle Menu
+                    </button>
+                    <li>Home</li>
+                    <li>Servies</li>
+                    <li>News</li>
+                    <li>Blog</li>
+                    <li>Contact</li>
+                </ul>
+            )}
         </div>
     );
 };
