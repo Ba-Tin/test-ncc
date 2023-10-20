10d8a2736af43c462ce526e4f3846b925e2f4e20
Tin Nguyen <nguyenbatin290900@gmail.com>
2023-10-20 09:30:12 +0700

    done

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,28 +1,34 @@
 import React, { useState } from 'react';
 import './style.css';
 
-const Menu = () => {
-    const [menuOpen, setMenuOpen] = useState(true);
-    const toggleMenu = () => {
-        setMenuOpen(!menuOpen);
-    };
-    return (
-        <div className='menu'>
+function Menu() {
+    const [clicked, setClicked] = useState(false);
 
-            {menuOpen && (
-                <ul>
-                    <button onClick={toggleMenu} className="menu-button">
-                        Toggle Menu
-                    </button>
+    const handleClick = () => {
+        setClicked(!clicked);
+    }
+
+    return (
+        <>
+            <div className='menu-container'>
+                <ul className={`menu ${clicked ? 'show' : ''}`}>
                     <li>Home</li>
                     <li>Servies</li>
                     <li>News</li>
                     <li>Blog</li>
                     <li>Contact</li>
                 </ul>
-            )}
-        </div>
+                <div id="mobile" onClick={handleClick}>
+                    <i
+                        id="bar"
+                        className={clicked ? 'fas fa-times' : 'fas fa-bars'}
+                        onClick={handleClick}
+                    ></i>
+                </div>
+            </div>
+
+        </>
     );
-};
+}
 
 export default Menu;
